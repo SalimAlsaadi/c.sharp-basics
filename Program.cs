15faf020aee693b65d6720be252f311063187029
SalimAlsaadi <al-saadis-s-s@hotmail.com>
2023-05-30 00:11:54 -0700

    Added basic information of the user.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Hello programmer");
+        Console.WriteLine("Name: salim");
+        Console.WriteLine("Father Name: Said");
+        Console.WriteLine("DOB: 1992-01-29");
+        Console.WriteLine("Email: al-saadis-s-s@hotmail.com");
+        Console.WriteLine("GitHub Username: SalimAlsaadi");
+        Console.WriteLine(" " + " " + " " + "*" + "" + " ");
+        Console.WriteLine(" " + " " + "*" + "*" + "*" + " ");
+        Console.WriteLine(" " + "*" + "*" + "*" + "*" + "*");
+        Console.WriteLine("*" + "*" + "*" + "*" + "*" + "*" + "*");
+        Console.WriteLine(" " + "*" + "*" + "*" + "*" + "*");
+        Console.WriteLine(" " + " " + "*" + "*" + "*" + " ");
+        Console.WriteLine(" " + " " + " " + "*" + "" + " ");
+
+
+
+
+
+
+
+    }
+}
